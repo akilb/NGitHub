a1b13bb3515735b774256eb54f7b3124c235547c
Akil Burgess <akilburgess@gmail.com>
2011-05-24 16:16:04 +0100

    Added WP7 and SL4 projects

--- src/NGitHub.WindowsPhone/Properties/AssemblyInfo.cs
+++ src/NGitHub.WindowsPhone/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NGitHub")]
+[assembly: AssemblyTitle("NGitHub.WindowsPhone")]
 [assembly: AssemblyDescription("Simple GitHub API wrapper for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NGitHub")]
+[assembly: AssemblyProduct("NGitHub.WindowsPhone")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed6ecf78-b6b3-4dfd-aecf-42586918ffdf")]
+[assembly: Guid("a07fbdfd-b450-41b6-9444-301562f1220f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,10 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
 [assembly: AssemblyFileVersion("0.1.*")]
-
-[assembly: InternalsVisibleTo("NGitHub.Test")]
